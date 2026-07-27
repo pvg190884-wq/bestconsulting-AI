e6ce8b66cee2e3e1c9a416e6582fa8d20471a427
pvg190884-wq <pvg190884@gmail.com>
2026-07-28 01:08:41 +0300

    register models
    register models

---
+++ app/db/models/__init__.py
@@ -0,0 +1,4 @@
+"""Импортирует все модели, чтобы SQLAlchemy знал о них при создании таблиц."""
+from app.db.models.client import Client
+from app.db.models.chat import ChatSession, ChatMessage, MessageRoleEnum
+from app.db.models.knowledge import KnowledgeItem, KnowledgeTypeEnum
